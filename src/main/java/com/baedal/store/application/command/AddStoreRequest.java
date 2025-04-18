30a05696382116cc7fc2c0b15426c00cf3455e7f
limjintaek <jintaek019@gmail.com>
2025-04-18 16:59:39 +0900

    [#3] add. StoreEntity 배달비 컬럼 추가

--- src/main/java/com/baedal/store/application/command/AddStoreRequest.java
+++ src/main/java/com/baedal/store/application/command/AddStoreRequest.java
@@ -2,6 +2,7 @@ package com.baedal.store.application.command;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -37,6 +38,10 @@ public class AddStoreRequest {
   @Schema(description = "영업 종료 시간")
   @NotBlank
   private LocalTime closeTime;
+
+  @Schema(description = "배달비")
+  @PositiveOrZero
+  private int deliveryAmount;
 
 
 }
--- src/main/java/com/baedal/store/domain/model/Store.java
+++ src/main/java/com/baedal/store/domain/model/Store.java
@@ -23,4 +23,6 @@ public class Store {
   private LocalTime openTime;
 
   private LocalTime closeTime;
+
+  private int deliveryAmount;
 }
