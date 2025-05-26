7e66ee6de81d626e52d29c888fe8287651acaa93
limjintaek <jintaek019@gmail.com>
2025-05-26 20:04:56 +0900

    add. 매장명 검색 한글 형태소 변환기 설정 추가

--- src/main/java/com/baedal/store/adapter/out/persistence/entity/StoreNameEntity.java
+++ src/main/java/com/baedal/store/adapter/out/persistence/entity/StoreNameEntity.java
@@ -4,8 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Document(indexName = "store_name")
+@Setting(settingPath = "/static/elastic/elastic-settings.json")
+@Mapping(mappingPath = "/static/elastic/store-name-mappings.json")
 @Builder
 @Getter
 public class StoreNameEntity {
@@ -13,6 +19,7 @@ public class StoreNameEntity {
   @Id
   private Long id;
 
+  @Field(name = "name", type = FieldType.Text)
   private String name;
 
 }
